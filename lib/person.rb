59068ccacc3523ec13b7a744c2b64173455325bd
Donna Amos <donnacamos.developer@gmail.com>
2019-04-08 15:03:30 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
-class Person
+class Person # Person is an object 
 end
 
-adele_goldberg = Person.new 
+adele_goldberg = Person.new # .new is the instance of the object Person. adele_goldberg is a variable you've defined as an instance of a Person. You've used an instance .new to make the object Person do something. This is a good example of how an object can be used to do something with an instance. Object is like a noun, instance is the verb. 
 alan_kay = Person.new 

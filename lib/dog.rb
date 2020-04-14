cfde79c0ccc460518b02a57c31d0d1fae1cb2a2a
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-14 21:18:06 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog 
+  
+  def puppy(x) 
+    puts "Woof" 
+  end
+  
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person 
+  
+  def dude(x)
+    puts "Hi!"
+  end 
+  
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new

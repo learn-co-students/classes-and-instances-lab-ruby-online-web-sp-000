42e4518e9898b17df228486bc17cc91ee89477aa
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 00:55:43 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,11 @@
 class Dog
-
-fido = dog.new
-snoopy = dog.new
-lassie = dog.new
-
 end
+
+fido = Dog.new
+fido #=> #<Dog:0x007fc52c2d7d20>
+
+snoopy = Dog.new
+snoopy #=> #<Dog:0x007fc52c2d4170>
+
+lassie = Dog.new
+lassie #=> #<Dog:0x007fc52c2cc588>
